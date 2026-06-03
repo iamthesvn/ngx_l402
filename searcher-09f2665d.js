0051650f367fc29346726f51ba2858b29b0b4c34
iamthesvn <iamthesvn@users.noreply.github.com>
2026-06-03 17:00:44 +0000

    deploy: fda557dc93c4a6b7b3bfc8b40931979fca8e0bac

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-98fd63e4.js',
+                path_to_root + 'searchindex-69be7e59.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
